47c8ffb1e9da63ba3a417370037fcc862244cf3a
Rosa-Maria Makinen <rmakinen@c1r6p9.hive.fi>
2024-04-04 19:11:51 +0300

    fixed regex, fixed edge cases

--- CPP09/ex00/main.cpp
+++ CPP09/ex00/main.cpp
@@ -1,7 +1,14 @@
 #include "BitcoinExchange.hpp"
 
-int main()
+int main(int argc, char **argv)
 {
+	if(argc == 2)
+	{
+		BitcoinExchange bt;
 
-	
+		bt.CalculateBitcoinExchange(argv[1]);
+
+		return 0;
+	}
+
 }
