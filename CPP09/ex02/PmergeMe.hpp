108cc7056fbf3a9664a4c65f5437f6de082fd689
Rosa-Maria Makinen <rmakinen@c1r5p9.hive.fi>
2024-04-11 11:58:32 +0300

    made validate input

--- CPP09/ex02/PmergeMe.hpp
+++ CPP09/ex02/PmergeMe.hpp
@@ -1,27 +1,55 @@
-#ifndef PMERGEME_H
-#define PMERGEME_H
+#pragma once
 
 #include <vector>
 #include <deque>
 #include <iostream>
-#include <exception>
+#include <stdexcept>
+#include <cctype>
+#include <string>
 
 class PmergeMe
 {
 
 	public:
 		template <template <typename...> class Container>
-		static void printContainer(const Container<int> &container);
+		static Container<unsigned int> validateInputCreateContainer(int argc, char **argv)
+		{
+			Container<unsigned int> holder;
+
+			for (int i = 1; i < argc; i++)
+			{
+					int number = std::stoi(argv[i]);
+					if (number >= 0)
+						holder.push_back(number);
+					else
+						throw std::logic_error("allowed only digits as input");
+			}
+			return holder;
+		};
+
+		template <template <typename...> class Container>
+		static void printContainer(const Container<unsigned int> &container)
+		{
+			{
+				for (const auto& it : container)
+				{
+					std::cout << it << std::endl;
+				}
+			}
+		};
+
+		template <template <typename...> class Container>
+		static void mergeInsertionSort(const Container<unsigned int> &container)
+		{
+			Container <std::pair<unsigned int, unsigned int>> pairs;
+			Container::iterator
+		};
 
 		// template <template <typename...> class Container>
-		// static void mergeInsertionSort(const Container<int> &container);
+		// static void mergeInsertionSort(const Container<int> &container)
+		// {
+		// 		Container<std::pair<int, int>> pairs;
 
-
-
-
+		// };
 
 };
-
-#include "./PmergeMe.tpp"
-
-#endif
--- CPP09/ex02/main.cpp
+++ CPP09/ex02/main.cpp
@@ -1,14 +1,24 @@
 #include <iostream>
 #include "PmergeMe.hpp"
 
-int	main()
+int	main(int argc, char **argv)
 {
-	std::vector<int> vec = {1, 2, 3, 4, 5};
-	PmergeMe::printContainer(vec);
+	try
+	{
+		std::vector<int> vec = PmergeMe::validateInputCreateContainer<std::vector>(argc, argv);
+		PmergeMe::printContainer(vec);
+	}
+	catch (std::logic_error)
+	{
 
-	std::deque<int> dec = {8, 8, 9, 6, 10};
-	PmergeMe::printContainer(dec);
+	}
+	// std::cout << "\nvector:: \n" << std::endl;
+	// std::vector<int> vec = {1, 2, 3, 4, 5};
 
+	// std::cout << "\ndeque:: \n" << std::endl;
+
+	// std::deque<int> dec = {8, 8, 9, 6, 10};
+	// PmergeMe::printContainer(dec);
 
 
 	// PmergeMe::mergeInsertionSort(vec);
