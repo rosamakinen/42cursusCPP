b0bd3b31d254dba3aac646a2f4ca700f13b40ae0
rosamakinen <112611789+rosamakinen@users.noreply.github.com>
2024-04-17 16:04:11 +0300

    fix merge sort issues

--- CPP09/ex02/PmergeMe.hpp
+++ CPP09/ex02/PmergeMe.hpp
@@ -26,69 +26,75 @@ class PmergeMe
 		}
 
 		template <template <typename...> class Container>
-		static Container<unsigned int> merge(Container<unsigned int> &mainChain, unsigned int start, unsigned int middle, unsigned int end)
+		static void merge(Container<unsigned int> &mainChain, unsigned int start, unsigned int middle, unsigned int end)
 		{
-			unsigned int leftSize = middle - start + 1; //up untill, including the middle
-			unsigned int rightSize = end - middle; // after middle
-			Container<unsigned int> rightChain;
-			Container<unsigned int> leftChain;
-			for (unsigned int i = 0; i < mainChain.size(); i++)
+			unsigned int leftSize = middle - start + 1;
+			unsigned int rightSize = end - middle;
+
+			Container<unsigned int> rightChain(rightSize);
+			Container<unsigned int> leftChain(leftSize);
+			for (unsigned int i = 0; i < leftSize; i++)
 			{
-				if (i <= middle)
-					leftChain.push_back(mainChain[i]);
-				else
-					rightChain.push_back(mainChain[i]);
+				leftChain[i] = mainChain[i + start];
 			}
 
+			for (unsigned int j = 0; j < rightSize; j++)
+			{
+				rightChain[j] = mainChain[middle + j + 1];
+			}
 			unsigned int i = 0;
 			unsigned int j = 0;
-			Container<unsigned int> mergedChain;
+			unsigned int k = start;
+	
 			while (i < leftSize && j < rightSize)
 			{
 				if (leftChain[i] <= rightChain[j])
-				{
-					mergedChain.push_back(leftChain[i]);
+				{    
+					mainChain[k] = leftChain[i];
 					i++;
 				}
 				else
 				{
-					mergedChain.push_back(rightChain[j]);
+					mainChain[k] = rightChain[j];
 					j++;
 				}
+				k++;
 			}
-			while(j < rightSize) //start from right side
-			{
-				mergedChain.push_back(rightChain[j]);
-				j++;
-			}
+
 			while(i < leftSize)
 			{
-				mergedChain.push_back(leftChain[i]);
+				mainChain[k] = leftChain[i];
 				i++;
+				k++;
 			}
-			return mergedChain;
+			while(j < rightSize)
+			{
+				mainChain[k] = rightChain[j];
+				j++;
+				k++;
+			}
+			std::cout << "after a round of sorting " << std::endl;
+			PmergeMe::printContainer(mainChain);
 		}
 
 		template <template <typename...> class Container>
-		static Container<unsigned int> sortMainChain(Container<unsigned int> &mainChain, unsigned int start, unsigned int end)
+		static void sortMainChain(Container<unsigned int> &mainChain, unsigned int start, unsigned int end)
 		{
 			Container<unsigned int> temp;
-			if (start >= end) // base case
-				return temp;
-
-			unsigned int middle = (start + (end - start)) / 2; // find midpoint
-			sortMainChain(mainChain, start, middle); // sort left side, to the left of the midpoint
-			sortMainChain(mainChain, middle + 1, end); // sort right side, to the right of the midpoint
-			temp = merge(mainChain, start, middle, end);
-			return temp;
+			unsigned int middle = start + (end - start) / 2;
+			std::cout << "start: " << start << " middle: " << middle << " end: " << end << std::endl;
+			if (start >= end || middle == end)
+				return;
+			
+			sortMainChain(mainChain, start, middle);
+			sortMainChain(mainChain, middle + 1, end);
+			merge(mainChain, start, middle, end);
+			return;
 		}
 
 		template <template <typename...> class Container>
 		static Container<unsigned int> insertionSort(Container<unsigned int> &preSorted, Container<unsigned int> bChain)
 		{
-			std::cout << "from merge insertion, preSorted: " << std::endl;
-			PmergeMe::printContainer(preSorted);
-			
 			unsigned int key;
 
 			for (unsigned int i = 0; i < bChain.size(); i++) 
@@ -101,14 +107,13 @@ class PmergeMe
 				if (key < preSorted[0])
 				{
 					preSorted.insert(preSorted.begin(), key);
-					break;
+					continue;
 				}
-				for (unsigned int j = size - 2; j >= 0; j--) // take into account the dirst element?
+				for (unsigned int j = size - 2; j >= 0; j--)
 				{
 					if (preSorted[j] < key && preSorted[j + 1] > key)
 					{
 						std::cout << "looping: key is: " << key << " j & j + 1 : " << preSorted[j] << ", " << preSorted[j + 1] << std::endl;
-						//typename Container<unsigned int>::iterator it = preSorted.begin();
 						preSorted.insert(preSorted.begin() + j + 1, key);
 						break;
 					}
@@ -149,10 +154,8 @@ class PmergeMe
 			if (container.size() % 2 != 0)
 				bChain.push_back(container.back());
 
-			std::cout << "before merge insertion" << std::endl;
-			PmergeMe::printContainer(aChain);
-			Container<unsigned int> preSorted = PmergeMe::sortMainChain(aChain, 0, aChain.size() - 1); //recursively sort a Chain by max value
-			Container<unsigned int> sorted = PmergeMe::insertionSort(preSorted, bChain);
+			PmergeMe::sortMainChain(aChain, 0, aChain.size() - 1); //recursively sort a Chain by max value
+			Container<unsigned int> sorted = PmergeMe::insertionSort(aChain, bChain);
 		}
 
 		template <template <typename...> class Container>
